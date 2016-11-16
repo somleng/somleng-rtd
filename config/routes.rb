5d8e8ea157d50cc54f86809a57cffba934e71a3f
David Wilkie <dwilkie@gmail.com>
2016-11-16 16:16:10 +0700

    Add temporary redirect from / to https://github.com/dwilkie/somleng-rtd. #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => redirect('https://github.com/dwilkie/somleng-rtd')
+
   namespace "api", :defaults => { :format => "json" } do
     resources :projects, :only => [:index, :show]
     resource  :real_time_data, :only => :show
