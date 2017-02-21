03d85eb6d7cf2347eb3361b3019c8614aec4b2b8
David Wilkie <dwilkie@gmail.com>
2017-02-21 11:39:09 +0700

    Return consistant order of projects on homepage. Closes #23

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def show
     @real_time_data = RealTimeData.new
-    @projects = Project.all.includes(:twilio_price)
+    @projects = Project.order(:created_at).includes(:twilio_price)
     @sample_project = @projects.first
   end
 end
